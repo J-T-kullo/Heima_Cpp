a5686ff93bbc1546086c4f9c4d0a31d68c97f978
Kullo <1324134263@qq.com>
2024-09-09 08:50:30 +0800

    Merge branch 'master' of https://github.com/J-T-kullo/Heima_Cpp

---
+++ C++_print_Helloworld/C++_print_Helloworld/28_combine_cycle.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+/*
+Ç¶Ì×Ñ­»·
+*/
+int main28() {
+	//´òÓ¡ÐÇÍ¼
+	for (int j = 0; j < 10; j++) {
+		for (int i = 0; i < 10; i++) {
+			cout << "* " ;
+		}
+		cout << endl;
+	}
+
+	cout << "\n" << endl;
+
+
+	system("pause");
+	return 0;
+}
